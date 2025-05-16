497d61bb892b8ebdf80d3ba03d7eeacfbb05b302
GuilhermeSallesBlanco <guilherme.salles.blanco@gmail.com>
2025-05-15 21:05:02 -0300

    Adicionando tarefas realizadas

--- encoder.v
+++ encoder.v
@@ -8,5 +8,39 @@ module Encoder (
     output reg A,
     output reg B
 );
+
+reg [1:0] estado;
+
+parameter S0 = 2'b00, // 00
+          S1 = 2'b01, // 01
+          S2 = 2'b10, // 10
+          S3 = 2'b11; // 11
     
+always @(negedge rst_n or posedge clk) begin 
+    if(!rst_n) begin
+        estado <= S0;
+    end else if (horario && !antihorario) begin // Lógica para movimento horário
+        case(estado)
+            S0: estado = S2;
+            S1: estado = S0;
+            S2: estado = S3;
+            S3: estado = S1;
+            default: estado = S0;
+        endcase
+    end else if (!horario && antihorario) begin // Lógica para movimento anti-horário
+        case(estado)
+            S0: estado = S1;
+            S1: estado = S3;
+            S2: estado = S0;
+            S3: estado = S2;
+            default: estado = S0;
+        endcase
+    end
+end
+
+always @(*) begin
+    A = estado[1]; // A é o bit mais significativo do estado
+    B = estado[0]; // B é o bit menos significativo do estado
+end
+
 endmodule
--- read_encoder.v
+++ read_encoder.v
@@ -5,7 +5,39 @@ module Read_Encoder (
     input wire A,
     input wire B,
 
-    output reg [1:0] dir
+    output reg [1:0] dir // 00 = Sem movimento, 01 = Horário, 10 = Anti-horário
 );
-    
+
+// Sentido horário: 00 → 10 → 11 → 01 → 00
+// Sentido anti-horário: 00 → 01 → 11 → 10 → 00
+
+reg[1:0] estadoAnterior;
+parameter hor = 2'b01; // Horário
+parameter ant = 2'b10; // Anti-horário
+
+always @(negedge rst_n or posedge clk) begin 
+    if(!rst_n) begin
+        estadoAnterior <= 2'b00;
+        dir <= 2'b00;
+    end else begin
+        case({estadoAnterior, {A,B}}) // Junta os 2 bits do estado anterior com os 2 bits de A e B
+            4'b0001, // 00 -> 01
+            4'b0111, // 01 -> 11
+            4'b1110, // 11 -> 10
+            4'b1000: // 10 -> 00
+            dir <= ant; // Anti-horário
+
+            4'b0010, // 00 -> 10
+            4'b1011, // 10 -> 11
+            4'b1101, // 11 -> 01
+            4'b0100: // 01 -> 00
+            dir <= hor; // Horário
+
+            default:
+            dir <= 2'b00; // Sem movimento reconhecido
+        endcase
+        estadoAnterior <= {A,B}; // Atualiza o estado anterior
+    end
+end
+
 endmodule
